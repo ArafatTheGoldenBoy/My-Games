774010fd9d8d88256eb5bde9da031ea9602991a9
Yasin Arafat <mdyasinarafat3@gmail.com>
2023-07-13 13:28:13 +0200

    remove unnecessary print statement

--- PurposeOfLife/Main.py
+++ PurposeOfLife/Main.py
@@ -4,22 +4,6 @@ from Player import Player
 def main():
     player1 = Player("Yasin", 29, "Male")
     player1.random_death()
-    print(
-        "Name:",
-        player1.name,
-        "\n",
-        "Age:",
-        player1.age,
-        "\n",
-        "Good Deeds:",
-        player1.good_deeds,
-        "\n",
-        "Bad Deeds:",
-        player1.bad_deeds,
-        "\n",
-        "Money:",
-        player1.money,
-    )
     print("Player's total lifetime is ", player1.time_line)
     # -------------Testing----------------#
     # ---- Player Start playing here------#
@@ -30,9 +14,10 @@ def main():
             player1.is_alive = False
             break
     # ---------End Testing----------------#
-    print(player1.age)
     print("Player is alive: ", player1.is_alive)
     player1.calculate_deeds()
+    print("righteous deeds are : ", player1.good_life_history)
+    print("Sins are : ", player1.bad_life_history)
 
 
 main()
