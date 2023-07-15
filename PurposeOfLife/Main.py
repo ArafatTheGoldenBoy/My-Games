c37601374297757263422b545bef754b39939bcd
Yasin Arafat <mdyasinarafat3@gmail.com>
2023-07-15 23:50:44 +0200

    adding interval to the game so that we can see what's happening with the character

--- PurposeOfLife/Main.py
+++ PurposeOfLife/Main.py
@@ -1,23 +1,28 @@
+import time
 from Player import Player
 
 
 def main():
-    player1 = Player("Yasin", 29, "Male")
+    player1 = Player("Yasin", 28, "Male")
     player1.random_death()
     print("Player's total lifetime is ", player1.time_line)
     # -------------Testing----------------#
     # ---- Player Start playing here------#
     for timeline in range(player1.time_line):
         player1.daily_action()
+        print("At the age of : ", player1.age)
+        print("righteous deeds are : ", player1.good_life_history)
+        print("Sins are : ", player1.bad_life_history)
+        print("rightous deeds point: ", player1.good_deeds)
+        print("sin point: ", player1.bad_deeds)
         player1.aging()
         if player1.age == player1.time_line:
             player1.is_alive = False
             break
+        time.sleep(2)
     # ---------End Testing----------------#
     print("Player is alive: ", player1.is_alive)
     player1.calculate_deeds()
-    print("righteous deeds are : ", player1.good_life_history)
-    print("Sins are : ", player1.bad_life_history)
 
 
 main()
