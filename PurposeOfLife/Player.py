3df7162da9127e1f8acb978b68d7fb43f7a7dc7a
Yasin Arafat <mdyasinarafat3@gmail.com>
2023-07-16 18:10:36 +0200

    Update Player.py
    Fix calculation

--- PurposeOfLife/Player.py
+++ PurposeOfLife/Player.py
@@ -35,32 +35,40 @@ class Player:
             print("He died at age of ", self.age)
 
     def random_death(self):
-        random_age = randrange(self.age + 1, 60)
+        # random_age = randrange(self.age + 1, 60)
+        random_age = 36
         self.time_line = random_age
         print("He will die at ", random_age)
 
     def daily_action(self):
-        rd = randrange(-7, 7)
+        rd = -7
         if rd > 0:
             self.good_deeds += rd
+            # ---- common situation for each timeline ----- #
+            # ---- That means if anyone want to forgive then the sin become zero and----- #
+            # ---- bad deeds counts as good deeds according 25-Surah Al-Furqan (The Criterion ) 70----- #
+            # 6 becouse he did repent, Allah forgive him :)
+            if rd == 6:
+                self.good_deeds += -self.bad_deeds
+                self.bad_deeds = 0
             self.good_life_history.append(self.good_action[rd])
+            self.good_life_history.append(rd)
         elif rd == 0:
             pass
         else:
             self.bad_deeds += rd
-            self.bad_life_history.append(self.bad_action[rd])
-            # ---- common situation for each timeline ----- #
-            # ---- That means if anyone want to forgive then the sin become zero and----- #
-            # ---- bad deeds counts as good deeds according 25-Surah Al-Furqan (The Criterion ) 70----- #
-            repent = randrange(0, 20)
-            # 7 becouse his repent, Allah forgive him :)
-            if repent == 7:
-                self.good_deeds += self.bad_deeds
-                self.bad_deeds = 0
+            self.bad_life_history.append(self.bad_action[-rd])
+            self.bad_life_history.append(-rd)
+            if self.time_line == self.age + 1 and rd == -7:
+                print(
+                    "If Allah forgive than his destination will be paradise",
+                    self.time_line,
+                    self.age,
+                )
             # ---- End common situation ---- #
 
     def calculate_deeds(self):
-        total_deeds = self.good_deeds - self.bad_deeds
+        total_deeds = self.good_deeds + self.bad_deeds
         print("amol nama : ", total_deeds)
         if total_deeds >= 0:
             print("Final destination: Paradise")
